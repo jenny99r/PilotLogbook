0757c3acd160790cb33a16f0edbb50f949e53ffe
Jenny <jenny99r@gmail.com>
2012-03-21 21:34:05 +0000

    Initial project added a from textview

---
+++ src/com/insane/jam/ferry/PilotLogActivity.java
@@ -0,0 +1,13 @@
+package com.insane.jam.ferry;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PilotLogActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
