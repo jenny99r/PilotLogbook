6f8316d8bf54d588bcd52de23a20d31341397342
Jenny <jenny99r@gmail.com>
2012-04-14 12:50:51 +0100

    Added more fields and initial work on date picker

--- gen/com/insane/jam/ferry/R.java
+++ gen/com/insane/jam/ferry/R.java
@@ -14,14 +14,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int flightDate=0x7f050009;
         public static final int fromAirfield=0x7f050003;
         public static final int tableLayout1=0x7f050000;
         public static final int tableRow1=0x7f050004;
         public static final int tableRow2=0x7f050001;
         public static final int tableRow3=0x7f050007;
-        public static final int tableRow4=0x7f050008;
+        public static final int tableRow4=0x7f05000a;
+        public static final int tableRow5=0x7f05000c;
+        public static final int tableRow6=0x7f05000e;
         public static final int textView1=0x7f050002;
         public static final int textView2=0x7f050005;
+        public static final int textView3=0x7f050008;
+        public static final int textView5=0x7f05000b;
+        public static final int textView6=0x7f05000d;
+        public static final int textView7=0x7f05000f;
         public static final int toAirfield=0x7f050006;
     }
     public static final class layout {
@@ -30,8 +37,12 @@ public final class R {
     }
     public static final class string {
         public static final int FromAirfield=0x7f040002;
+        public static final int On=0x7f040004;
         public static final int ToAirfield=0x7f040003;
+        public static final int aircraftReg=0x7f040007;
         public static final int app_name=0x7f040001;
+        public static final int breaksOffTime=0x7f040005;
+        public static final int breaksOnTime=0x7f040006;
         public static final int hello=0x7f040000;
     }
 }
